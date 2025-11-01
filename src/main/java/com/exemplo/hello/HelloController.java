e6dd6f4427d6c88e5f0b557a1f6de1bb1211c550
Davi Resende <davigomides161@gmail.com>
2025-11-01 11:06:59 -0300

    adicionar prática completa
    trabalho completo já feito

---
+++ src/main/java/com/exemplo/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.exemplo.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+    
+    @GetMapping("/hello/{nome}")
+    public String helloNome(@PathVariable String nome) {
+        return "Hello World, " + nome;
+    }
+}
